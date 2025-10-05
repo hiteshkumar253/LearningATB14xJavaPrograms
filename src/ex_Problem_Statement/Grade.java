3ae73283f0880e43848663c2fa75afce25ffe31c
Hitesh_Kumar <hiteshkumar253@gmail.com>
2025-10-05 19:27:40 +0530

    Fizzbuzz and Grade Program

---
+++ src/ex_Problem_Statement/Grade.java
@@ -0,0 +1,37 @@
+package ex_Problem_Statement;
+
+import java.util.Scanner;
+
+public class Grade {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int num;
+        while (true) {
+            if (scanner.hasNextInt()) {
+                num = scanner.nextInt();
+                if (num >= 0 && num <= 100) {
+                    break;
+                } else {
+                    System.out.println("Please enter a number between 0 and 100.");
+                }
+            }else {
+                System.out.println("Invalid number, please enter a valid number");
+                scanner.next(); // clear invalid input
+            }
+        }
+
+        if (num >= 90 && num <= 100) {
+            System.out.println("Grade : A");
+        } else if (num >= 80) {
+            System.out.println("Grade : B");
+        } else if (num >= 70) {
+            System.out.println("Grade : C");
+        } else if (num >= 60) {
+            System.out.println("Grade : D");
+        } else {
+            System.out.println("Grade : E");
+        }
+    }
+}
