32ad316aa4660b351e342f94d925458a03e5a272
Hitesh_Kumar <hiteshkumar253@gmail.com>
2025-10-09 00:16:36 +0530

    Code Comments Updated

--- src/ex_14_Strings/Lab101_String_Immutable.java
+++ src/ex_14_Strings/Lab101_String_Immutable.java
@@ -5,7 +5,7 @@ public class Lab101_String_Immutable {
 
         String name = "Hitesh";
         name.toUpperCase();
-        System.out.println(name); // which one this print Pramod or PRAMOD? Ans is Pramod
+        System.out.println(name); // which one this print Hitesh or HITESH? Ans is Hitesh
 //        System.out.println(name.toUpperCase()); //PRAMOD
     }
 }
